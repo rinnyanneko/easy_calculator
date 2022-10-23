44f57d945d37af898032d9aa3a0d31eb5e72feae
twmccreeper <74105315+twmccreeper@users.noreply.github.com>
2022-10-23 15:58:34 +0800

    lol

---
+++ easy_ calculator.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cmath>
+#define rad 0.01745329252
+using namespace std;
+ main()
+{
+  bool R=1;
+  long double A=0;
+  long double B=0;
+  char symbol;
+  cout<<"easy calculator"<<endl<<"credit:@twmccreeper"<<endl<<endl<<endl;
+  while (R)
+  {
+    A=0;
+    B=0;
+    symbol=0;
+    cout<<"Please Enter 1st Number"<<endl;
+    cin>>A;
+    cout<<"Please Enter Symbol of Operation"<<endl<<"+=plus, -=subtract, *=multiply, /=devide, ^=power"<<endl<<"1=sin(sinne), 2=cos(cosine), 3=tan(tangent), 4=ctg(cotangent), 5=sec(secant), 6=csc(cosecant)"<<endl;
+    cin>>symbol;
+    cout<<"Please Enter 2nd Number"<<endl;
+    cin>>B;
+
+    switch (symbol)
+      {
+      case '+':
+        A += B;
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '-':
+        A -= B;
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '*':
+        A *= B;
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '/':
+        A /= B;
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '^':
+        A=pow(A, B);
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '1':
+        A=sin(B*rad);
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '2':
+        A=cos(B*rad);
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '3':
+        A=tan(B*rad);
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '4':
+        A=1/tan(B*rad);
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '5':
+        A=1/cos(B*rad);
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      case '6':
+        A=sin(B*rad);
+        cout <<"The answer is "<<endl<< A << endl;
+        break;
+      default:
+        cout <<"ERROR: Operation Symbol Not Found"<< endl;
+      } 
+      
+    cout<<"Continue?"<<endl<<"1=continue"<<endl<<"0=exit"<<endl;
+    cin>>R;
+    if(R!=1)
+    {
+      R=0;
+      system("pause");
+    }
+  }
+}
